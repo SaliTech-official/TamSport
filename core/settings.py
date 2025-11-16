531fb84ead594a46c1a9d08b09e13dba14c300b1
alireza <165670900+alirezabaharloo@users.noreply.github.com>
2025-11-16 21:03:27 +0330

    Update settings.py (fix confug to config)

--- core/settings.py
+++ core/settings.py
@@ -102,7 +102,7 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # }
 
 DATABASES = {
-    'default': dj_database_url.confug(default=os.getenv("DATABASE_URL"))
+    'default': dj_database_url.config(default=os.getenv("DATABASE_URL"))
 }
 
 # Password validation
