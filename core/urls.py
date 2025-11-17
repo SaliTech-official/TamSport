8b525387df7c6da55220b0655d48980c581477df
alireza <alireza@gmail.com>
2025-11-17 20:50:28 +0330

    delete liara_nginx.conf and some changes

--- core/urls.py
+++ core/urls.py
@@ -2,10 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from rest_framework.documentation import include_docs_urls
 
 urlpatterns = [
-    path('', include_docs_urls(title='TAM API Documentation', public=True)),
     path('admin/', admin.site.urls),
     path('api/auth/', include('accounts.urls')),
     path('api/blog/', include('blog.urls')),
