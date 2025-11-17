50f23eb59c3ff6ea3a60a14059748819c8a20115
alireza <alireza@gmail.com>
2025-11-17 18:01:31 +0330

    preparing frontend files

--- vite.config.js
+++ vite.config.js
@@ -4,15 +4,15 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  // All config here is related to production build only
   build: {
-    outDir: 'dist',
-    sourcemap: true, // Enable source maps for debugging
+    outDir: 'build',
+    sourcemap: false, // Disable source maps for production
     minify: 'esbuild',
     rollupOptions: {
       output: {
         manualChunks: undefined, // Prevent chunk splitting issues
       }
     }
-  },
-
+  }
 })
