78689ceb60ff0d1884a774ae946e07bf4407a086
alireza <alireza@gmail.com>
2025-11-19 23:24:24 +0330

    remove "api" from all urls

--- core/urls.py
+++ core/urls.py
@@ -5,8 +5,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/auth/', include('accounts.urls')),
-    path('api/blog/', include('blog.urls')),
-    path('api/admin/', include('adminpanel.urls')),
+    path('auth/', include('accounts.urls')),
+    path('blog/', include('blog.urls')),
+    path('admin/', include('adminpanel.urls')),
     # path('rosetta/', include('rosetta.urls')),
 ]
