3f86b137e276c8d47794e5c27cca5216bf907d88
alireza <alireza@gmail.com>
2025-11-20 20:00:16 +0330

    update urls.py

--- core/urls.py
+++ core/urls.py
@@ -1,12 +1,12 @@
-from django.contrib import admin
+# from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('auth/', include('accounts.urls')),
     path('blog/', include('blog.urls')),
     path('admin/', include('adminpanel.urls')),
+    # path('admin/', admin.site.urls),
     # path('rosetta/', include('rosetta.urls')),
 ]
