5f2a6938e9aa468df15d862211aee784ab398bdd
alireza <alireza@gmail.com>
2025-11-20 20:56:26 +0330

    update nginx configurations

--- src/context/HomeContext.jsx
+++ src/context/HomeContext.jsx
@@ -24,7 +24,7 @@ export const HomeProvider = ({ children }) => {
     isError,
     data: response,
     sendRequest
-  } = useHttp(`api/blog/home-datas/`, false);
+  } = useHttp(`/api/blog/home-datas/`, false);
 
   useEffect(() => {
     if (response) {
